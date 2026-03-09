aae26bea50ef0d380f22e96ac66b184726865af2
Hamed Bouare <hamed.bouare@paris-iea.fr>
2026-03-09 19:12:17 +0100

    handle: favicon white and black

--- app/app.vue
+++ app/app.vue
@@ -5,9 +5,14 @@
 </template>
 
 <script setup lang="ts">
+const { isDark } = useAppTheme()
+
+const { baseURL } = useRuntimeConfig().app
+const faviconHref = computed(() => `${baseURL}${isDark.value ? 'favicon.svg' : 'favicon-light.svg'}`)
+
 useHead({
   link: [
-    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    { rel: 'icon', type: 'image/svg+xml', href: faviconHref },
   ],
 })
 </script>
