e42637f25dc41f9fcc8395a355abe8265808807f
Hamed Bouare <hamed.bouare@paris-iea.fr>
2026-03-12 20:47:45 +0100

    chore: remove background color

--- app/app.vue
+++ app/app.vue
@@ -5,13 +5,7 @@
 </template>
 
 <script setup lang="ts">
-const { isDark } = useAppTheme()
-
-useHead(() => ({
-  link: [
-    { rel: 'icon', type: 'image/svg+xml', href: isDark.value ? '/favicon-dark.svg' : '/favicon-light.svg', key: 'favicon' },
-  ],
-}))
+useAppTheme()
 
 // Fix #4: attendre que Vuetify ait peint les bonnes CSS variables avant de révéler.
 onMounted(() => {
